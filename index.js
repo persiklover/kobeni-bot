3afaaa2b2aa2333ab1dd117266a9800d5fe892c2
persiklover <dannniska@gmail.com>
2022-03-18 15:18:42 +0300

    Imports & executes commands

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const { Client } = require('discord.js');
 const bot = new Client();
 
 const fs = require('fs');
+const commands = fs.readdirSync('./commands')
+  .filter(file => file.endsWith('.js'))
+  .map(filename => require(`./commands/${filename}`));
 
 bot.on('ready', () => {
   console.log('Bot online');
@@ -9,6 +12,12 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   console.log(msg.content);
+
+  for (let command of commands) {
+    if (msg.content.match(command.name)) {
+      command.action(msg);
+    }
+  }
 });
 
 bot.login(fs.readFileSync('./token.txt', { encoding: 'utf-8' }));
