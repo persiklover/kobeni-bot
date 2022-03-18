349e97115155dd506dc9b56b6d785f71ff3fc731
persiklover <dannniska@gmail.com>
2022-03-18 15:48:38 +0300

    Add recursive commands import

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const { Client } = require('discord.js');
 const bot = new Client();
 
 const fs = require('fs');
-const commands = fs.readdirSync('./commands')
-  .filter(file => file.endsWith('.js'))
-  .map(filename => require(`./commands/${filename}`));
+
+function getCommands() {
+  const dir = './commands';
+  const dirs = fs.readdirSync(dir, { withFileTypes: true });
+  const files = dirs.map(dirent => {
+    const res = dirent.name;
+    return dirent.isDirectory() ? getCommands(res) : res;
+  });
+
+  return files
+    .filter(file => file.endsWith('.js'))
+    .map(filename => require(`./commands/${filename}`));
+}
+
+const commands = getCommands();
 
 bot.on('ready', () => {
   console.log('Bot online');
